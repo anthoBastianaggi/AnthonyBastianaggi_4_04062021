dee92529722f3ec7c7cc6c2849db04f90e63a5c2
Anthony Bastianaggi <a.bastianaggi@gmail.com>
2021-06-04 21:03:36 +0200

    [GA-1] (Add) Add function close modal if button X is clicked and if users clicks anywhere outside of the modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,25 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+modalClose.onclick = function() {
+  modalbg.style.display = "none";
+}
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalbg) {
+    modalbg.style.display = "none";
+  }
+}
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
