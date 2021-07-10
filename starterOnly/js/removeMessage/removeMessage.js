fa65ebf91b3b13a009cbbc7ea092fb2cef41b137
Anthony Bastianaggi <a.bastianaggi@gmail.com>
2021-07-10 15:50:31 +0200

    [GA-5] (Add) Check validation Birthdate and update function Validate

--- starterOnly/js/removeMessage/removeMessage.js
+++ starterOnly/js/removeMessage/removeMessage.js
@@ -1,4 +1,5 @@
 import { isString, isEmail, isNumber, removeInvalidMessage } from '../utils/utils.js';
+import { validateLocation } from '../validate/validate.js';
 
 // Remove class invalid element ( Firstname, Lastname ) if value element is validate 
 export function removeInvalidMessageName(element, regex, errorClassName) {
@@ -14,9 +15,34 @@ export function removeInvalidMessageEmail(element, errorClassName) {
     }  
 }
 
+// Remove class invalid element ( Birthdate ) if value element is validate 
+export function removeInvalidMessageBirthdate(element, regex, errorClassName) {
+    var userage = new Date(element.value).getFullYear();
+    var todayYear = new Date().getFullYear()
+    var cutOff8 = todayYear - 8;
+
+    if(!element.validity.valueMissing && regex.test(element.value) && userage <= cutOff8 && userage >= 1900) {
+        removeInvalidMessage(element, '', errorClassName, 'red')
+    }  
+}
+
 // Remove class invalid element ( Quantity ) if value element is validate 
 export function removeInvalidMessageQuantity(element, regex, errorClassName) {
     if(!element.validity.valueMissing && isNumber(element.value) && regex.test(element.value) && element.value.length >= 0 && element.value.length <= 99) {
+        removeInvalidMessage(element, '', errorClassName, 'red')
+    }  
+}
+
+// Remove class invalid element ( Location ) if value element is validate 
+export function removeInvalidMessageLocation(element, errorClassName) {
+    if(validateLocation) {
+        removeInvalidMessage(element, '', errorClassName, 'red')
+    }  
+}
+
+// Remove class invalid element ( CGU ) if value element is validate 
+export function removeInvalidMessageCGU(name, element, errorClassName) {
+    if(name.checked) {
         removeInvalidMessage(element, '', errorClassName, 'red')
     }  
 }
